a39711cbd63b2387c79829bd5237a98cee3ee964
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-05 19:01:51 +0100

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="shadow-xl fixed h-20 left-0 top-0 z-10 bg-[#000] text-white py-0 md:px-20 px-10 w-full">
+    <div class="flex flex-wrap center place-content-center">
+        <h1 class="text-[20px] pt-10 logo">  Movie app </h1>
+         <img src="https://media.giphy.com/media/mGcNjsfWAjY5AEZNw6/giphy.gif" width="60">
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
