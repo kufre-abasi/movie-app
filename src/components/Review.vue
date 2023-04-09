982351fe2573d3f75928bd9bdca8eac5bbbbd23d
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-09 22:50:14 +0100

    review

---
+++ src/components/Review.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <textarea name="" id="" cols="30" rows="10">
+            
+        </textarea>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
