fe7383e50e19f1fd186a9b360b2f5f2bb217afb5
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-15 15:05:31 +0100

    updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="shadow-xl fixed h-20 left-0 top-0 z-10 bg-[#000] text-white py-0 md:px-20 px-10 w-full">
+  <div class="shadow-2xl fixed h-20 left-0 top-0 z-10 bg-gray-900 text-white py-0 md:px-20 px-8 w-full">
     <div class="flex flex-wrap center place-content-center">
-        <h1 class="text-[20px] pt-10 logo">  Movie app </h1>
-         <img src="https://media.giphy.com/media/mGcNjsfWAjY5AEZNw6/giphy.gif" width="60">
+        <h1 class="text-[40px] place-center mt-2 logo">  Movie app </h1>
+         <img class="" src="@/assets/movie-logo.svg" width="60">
     </div>
   </div>
 </template>
