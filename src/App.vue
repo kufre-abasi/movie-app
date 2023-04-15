2648b032f43d36fd9cc81ce95b4e8bf6ae777d04
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-15 15:24:58 +0100

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/Navbar.vue';
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <div>
     <Navbar />
-    <div class="pt-[100px] lg:px-10 px-4">
+    <div class="pt-[100px] pb-[200px] lg:px-10 px-4">
     <RouterView />
     </div>
+    <Footer />
   </div>
 </template>
 
