61773c0695d9fa2f87bb559bc389bb66d8051d6f
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-15 15:24:36 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,38 @@
-<template>
-  
-</template>
-
 <script>
 export default {
-
-}
+	data: () => {
+		return {
+			copyrightDate: new Date().getFullYear(),
+			projectName: 'Movie App',
+			author: 'Kufre-abasi',
+		};
+	},
+};
 </script>
 
-<style>
+<template>
+	<div class="flex justify-center items-center content-end text-center ">
+		<div
+			class=" text-md text-gray-200"
+		>
+			&copy; {{ copyrightDate }}.
+			<a
+				href="https://github.com/kufre-abasi/hire-me"
+				target="__blank"
+				class="   text-brand  "
+			>
+				{{ projectName }}
+			</a>
+			. Developed by
+			<a
+				href="https://github.com/kufre-abasi/hire-me"
+				target="__blank"
+				class="  uppercase text-brand  "
+				>{{ author }}</a
+			>
+		</div>
+	</div>
+</template>
 
+<style  scoped>
 </style>
