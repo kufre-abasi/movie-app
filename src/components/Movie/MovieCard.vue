935ea8cbd5be38bd2c82e9770ba6a671cfb6828e
kufre-abasi <kufreabasibassey3@gmail.com>
2023-04-15 15:06:08 +0100

    modified

--- src/components/Movie/MovieCard.vue
+++ src/components/Movie/MovieCard.vue
@@ -1,19 +1,74 @@
 <template>
-    <div class="card shadow-xl rounded-[14px] h-auto w-60 p-[1px] bg-[#000] text-white w-full">
-        <div class="bg-[#000] rounded-[14px] p-2">
-            <div class="h-40 bg-white rounded-[10px] place-center">
-                <img src="https://media.giphy.com/media/mGcNjsfWAjY5AEZNw6/giphy.gif" width="60">
+    <div class="card drop-shadow-lg rounded-[14px] w-80 p-[1px] bg-gray-200 text-[#000] text-white">
+        <div class=" bg-transparent rounded-[14px] p-2">
+            <div class="lg:flex  justify-between gap-4">
+                <img :src="image" :alt="slug" width="" class="rounded-[14px] lg:w-[120px] w-full">
+                <div class="">
+                    <strong class="text-[17px]">{{title}}</strong>
+                    <span>({{ year }})</span>
+                    <div>{{ genre }}</div>
+                    <div v-for="rate in ratings" :key="rate">
+                        <span>
+                        {{ rate }}
+                        </span>
+                    </div>
+                </div>
             </div>
-            <div class=" pt-10">
-                <span>title</span>
-                <p>Lorem ipsum dolor sit ame ?</p>
-                    <div>rating *********</div>
-            </div>
+            <!-- <div class=" pt-10 justify ">
+                <div class="flex gap-2 mb-2">
+                    <strong class="text-[17px]">{{title}}</strong>
+                    <span>({{ year }})</span>
+                </div>
+                <p class="text-[14px]">{{ plot }}</p>
+            </div> -->
         </div>
     </div>
 </template>
 
 <script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    // plot: {
+    //   type: String,
+    //   required: true
+    // },
+    ratings: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    year: {
+     type: String,
+     required: true
+    },
+    slug: {
+     type: String,
+     required: true
+    },
+    genre: {
+     type: String,
+     required: true
+    },
+    // released: {
+    //  type: String,
+    //  required: true
+    // }
+  },
+  setup(props) {
+    return {
+      // Your component's logic here
+    }
+  }
+})
 </script>
 
 <style>
